3ed0faa70fc11c151bf778b32e04a3b95facd03b
raihven <raihven@protonmail.com>
2023-10-04 13:14:39 -0400

    fixed routing, about us page displays proper, site name in header disapears wtih smaller page width ...

--- src/Components/About Us/AboutUs.jsx
+++ src/Components/About Us/AboutUs.jsx
@@ -1,17 +1,17 @@
 // import React from 'react';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+//import Header from '../Header/Header';
+//import Footer from '../Footer/Footer';
 import './AboutUs.css';
 
 const AboutUs = () => {
     return (
         <div className="about-us">
-            <Header />
+            {/* <Header /> */}
             <div className="content">
                 <h1>About Us</h1>
                 {/* add content here */}
             </div>
-            <Footer />
+            {/* <Footer /> */}
         </div>
     );
 };
