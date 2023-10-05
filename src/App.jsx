b6cfe92c2fdd1567c92d48b0c770b96aac847fcc
raihven <raihven@protonmail.com>
2023-10-05 13:08:49 -0400

    added favicon header addtions & adj

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import Body from "./Components/About Us/AboutUs";
+import AboutUs from "./Components/About Us/AboutUs";
 
 function App() {
   return (
     <Router>
       <div className="white-gradient">
         <Header />
-        <Body />
+        <AboutUs />
         <Footer />
       </div>
     </Router>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,23 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
-// import DropdownMenu from './DropdownMenu';
-import { useState } from "react";
 
 const Header = () => {
-  // const dropdownOptions = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
   const [open, setOpen] = useState(false);
   return (
     <section className="h-wrapper">
       <div className="h-container">
+      <div className="logo-text-container">
         <img src="src/assets/gg_Logo.PNG" alt="Logo" />
-        <h1 className="primaryText">GadgetGalaxy</h1>
+        <h1 className="primaryText">
+          <span className="cooper-black">gadget</span>
+          <span className="orator-bold">GALAXY</span>
+        </h1>
+        </div>
         <div className="search-bar">
           <input type="text" placeholder="Search any product" />
         </div>
-        {/* ${}is string interpolation and otar bhitore conditional statement */}
-
-        <div className="h-menu ">
-          {/* <DropdownMenu options={dropdownOptions} /> */}
+        <div className="h-menu">
           <div className="menu-trigger" onClick={() => setOpen(!open)}>
             <h3>Products</h3>
             <div className={`dropdown-menu ${open ? "active" : "inactive"}`}>
@@ -28,7 +27,6 @@ const Header = () => {
               </ul>
             </div>
           </div>
-
           <h3>Contact Us</h3>
           <h3>Shopping Cart</h3>
         </div>
@@ -37,12 +35,10 @@ const Header = () => {
   );
 };
 
-function DropdownItem(props) {
-  return (
-    <li className="dropdownItem">
-      {/* <img src = {props.img}></img> */}
-      <a>{props.text}</a>
-    </li>
-  );
-}
+const DropdownItem = ({ text }) => (
+  <li className="dropdownItem">
+    <a>{text}</a>
+  </li>
+);
+
 export default Header;
